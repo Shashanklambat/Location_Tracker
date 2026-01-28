daff9bd6d56fe7ed652f052480953e221d477ea5
Shashanklambat <shashanklambat1@gmail.com>
2026-01-29 00:21:56 +0530

    smooth track

--- public/dashboard.js
+++ public/dashboard.js
@@ -10,30 +10,74 @@ const markers = {};
 const bounds = L.latLngBounds();
 let autoFit = true;
 
-const userListDiv = document.getElementById("userList");
-
-// Stop auto-zoom when admin interacts
-map.on("zoomstart mousedown", () => {
+// stop auto fit if admin touches map
+map.on("mousedown zoomstart", () => {
   autoFit = false;
 });
 
-// Receive live locations
-socket.on("locationBroadcast", data => {
-  const { user, lat, lng } = data;
-  const position = [lat, lng];
+// smooth pan to a user
+function smoothFocus(latlng) {
+  map.flyTo(latlng, Math.max(map.getZoom(), 16), {
+    animate: true,
+    duration: 1.2
+  });
+}
 
-  if (!markers[user]) {
-    const marker = L.marker(position).addTo(map);
-    marker.bindTooltip(user, {
-      permanent: true,
-      direction: "top"
-    });
-    markers[user] = marker;
-  } else {
-    markers[user].setLatLng(position);
+// animate marker movement
+function animateMarker(marker, from, to) {
+  let start = null;
+  const duration = 1000;
+
+  function step(ts) {
+    if (!start) start = ts;
+    const progress = Math.min((ts - start) / duration, 1);
+
+    const lat = from.lat + (to.lat - from.lat) * progress;
+    const lng = from.lng + (to.lng - from.lng) * progress;
+
+    marker.setLatLng([lat, lng]);
+
+    if (progress < 1) requestAnimationFrame(step);
   }
 
-  bounds.extend(position);
+  requestAnimationFrame(step);
+}
+
+// UI list
+const userListDiv = document.getElementById("userList");
+
+function renderUserList() {
+  userListDiv.innerHTML = "";
+  Object.keys(markers).forEach(user => {
+    const div = document.createElement("div");
+    div.className = "user";
+    div.innerText = user;
+
+    div.onclick = () => {
+      autoFit = false;
+      smoothFocus(markers[user].getLatLng());
+    };
+
+    userListDiv.appendChild(div);
+  });
+}
+
+// Receive GPS updates
+socket.on("locationBroadcast", data => {
+  const { user, lat, lng } = data;
+  const newPos = L.latLng(lat, lng);
+
+  if (!markers[user]) {
+    const marker = L.marker(newPos).addTo(map);
+    marker.bindTooltip(user, { permanent: true, direction: "top" });
+    markers[user] = marker;
+    bounds.extend(newPos);
+  } else {
+    const oldPos = markers[user].getLatLng();
+    animateMarker(markers[user], oldPos, newPos);
+  }
+
+  bounds.extend(newPos);
 
   if (autoFit) {
     map.fitBounds(bounds, { padding: [50, 50], maxZoom: 16 });
@@ -50,21 +94,3 @@ socket.on("userDisconnected", user => {
     renderUserList();
   }
 });
-
-// Render right-side live users list
-function renderUserList() {
-  userListDiv.innerHTML = "";
-
-  Object.keys(markers).forEach(user => {
-    const div = document.createElement("div");
-    div.className = "user";
-    div.innerText = user;
-
-    div.onclick = () => {
-      autoFit = false;
-      map.setView(markers[user].getLatLng(), 17);
-    };
-
-    userListDiv.appendChild(div);
-  });
-}
