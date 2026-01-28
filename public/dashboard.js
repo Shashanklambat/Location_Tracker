dcb9e7a2e56ac06a114ee535d8cd92fdc3c663ac
Shashanklambat <shashanklambat1@gmail.com>
2026-01-28 21:32:44 +0530

    location panels

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,12 +1,3 @@
-const userListEl = document.getElementById("users");
-
-
-let autoFit = true;
-
-map.on("mousedown zoomstart", () => {
-  autoFit = false; // admin took control
-});
-
 const map = L.map("map").setView([20, 78], 5);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -14,52 +5,66 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 const socket = io();
+
 const markers = {};
 const bounds = L.latLngBounds();
+let autoFit = true;
 
-socket.on("locationBroadcast", data => {
-  updateUserList();
-  const { user, lat, lng } = data;
-  const pos = [lat, lng];
+const userListDiv = document.getElementById("userList");
 
-  if (!markers[user]) {
-    const marker = L.marker(pos).addTo(map);
-
-    marker.bindTooltip(user, {
-      permanent: true,
-      direction: "top",
-      offset: [0, -10]
-    });
-
-    markers[user] = marker;
-  } else {
-    markers[user].setLatLng(pos);
-  }
-
-  bounds.extend(pos);
-
-  if (autoFit) {
-    map.fitBounds(bounds, {
-      padding: [60, 60],
-      maxZoom: 17
-    });
-  }
-
+// Stop auto-zoom when admin interacts
+map.on("zoomstart mousedown", () => {
+  autoFit = false;
 });
 
-function updateUserList() {
-  userListEl.innerHTML = "";
+// Receive live locations
+socket.on("locationBroadcast", data => {
+  const { user, lat, lng } = data;
+  const position = [lat, lng];
+
+  if (!markers[user]) {
+    const marker = L.marker(position).addTo(map);
+    marker.bindTooltip(user, {
+      permanent: true,
+      direction: "top"
+    });
+    markers[user] = marker;
+  } else {
+    markers[user].setLatLng(position);
+  }
+
+  bounds.extend(position);
+
+  if (autoFit) {
+    map.fitBounds(bounds, { padding: [50, 50], maxZoom: 16 });
+  }
+
+  renderUserList();
+});
+
+// Remove user on disconnect
+socket.on("userDisconnected", user => {
+  if (markers[user]) {
+    map.removeLayer(markers[user]);
+    delete markers[user];
+    renderUserList();
+  }
+});
+
+// Render right-side live users list
+function renderUserList() {
+  userListDiv.innerHTML = "";
 
   Object.keys(markers).forEach(user => {
-    const li = document.createElement("li");
-    li.style.cursor = "pointer";
-    li.innerText = user;
+    const div = document.createElement("div");
+    div.className = "user";
+    div.innerText = user;
 
-    li.onclick = () => {
+    div.onclick = () => {
       autoFit = false;
       map.setView(markers[user].getLatLng(), 17);
     };
 
-    userListEl.appendChild(li);
+    userListDiv.appendChild(div);
   });
 }
--- server.js
+++ server.js
@@ -7,32 +7,28 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Default route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Socket.IO logic
 io.on("connection", socket => {
+  console.log("User connected");
+
   socket.on("sendLocation", data => {
-    socket.user = data.user;
+    socket.username = data.user;
     io.emit("locationBroadcast", data);
   });
 
-  socket.on("userDisconnected", user => {
-  if (markers[user]) {
-    map.removeLayer(markers[user]);
-    delete markers[user];
-    updateUserList();
-  }
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      io.emit("userDisconnected", socket.username);
+    }
+    console.log("User disconnected");
   });
 });
 
-
-// 🚨 THIS PART IS CRITICAL FOR RENDER
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
