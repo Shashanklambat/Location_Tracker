38a67dfb7f1fc8253bd84e9e03b1e56e8d434534
Shashanklambat <shashanklambat1@gmail.com>
2026-01-28 20:12:14 +0530

    server.js new

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
+// Default route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Socket.IO logic
 io.on("connection", socket => {
-  socket.on("locationUpdate", data => {
+  console.log("User connected");
+
+  socket.on("sendLocation", data => {
     io.emit("locationBroadcast", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
 
+// 🚨 THIS PART IS CRITICAL FOR RENDER
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
